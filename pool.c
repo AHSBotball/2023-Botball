c86e68e05a8948157663d7f409e8f62436c7e5be
AHSBotball <129643474+AHSBotball@users.noreply.github.com>
2023-04-10 14:52:36 -0400

    Updating code and removing useless code
    changed the port of the light sensor because 2 was acting weird. Added some sleeps in the code to make sure the noodle doesn't go flying when it's released. Also removed the whole pom finding code because it was bad and I want to remake it. Also adjusted some of the turns to make them better. Also made the robot turn around right after it grabs the noodle so I don't have to turn when placing the noodle down. :)

--- pool.c
+++ pool.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-    wait_for_light(2);
+    wait_for_light(3);
     shut_down_in(115);
 	
     msleep(5000); //delay for icreate to start
@@ -10,7 +10,7 @@ int main()
     cmpc(3); 
     enable_servo(1);
     enable_servo(2);
-    set_servo_position(1,550);
+    set_servo_position(1,600);
     set_servo_position(2, 1600);
    
         while (gmpc(3)<1720) // goes forward a bit 
@@ -22,18 +22,18 @@ int main()
     msleep(1000); // pause for a bit
     
     cmpc(3);
-    while (gmpc(3)<4125)
+    while (gmpc(3)<4200)
     {
         motor(3,100); // turn 270 deg
     }
     ao();
     msleep(1000);
     
-       while(digital(0) == 0) // will always equal 0
+    while(digital(0) == 0) // will always equal 0
     { 
         if(analog(0) > 2900) //if proximity sensor close 
         {
-            msleep(500); // pause 
+            msleep(400);// pause 
             ao();
             msleep(2000);
             set_servo_position(2, 2000); // close the claw
@@ -41,62 +41,50 @@ int main()
             set_servo_position(1, 0); //lift the noodle
             msleep(1000);
             
-            motor(0, -100); //go backwards
+            motor(0, -100); //back up a bit b4 turning around
             motor(3, -100);
-            msleep(2500);
-                
-            set_servo_position(1, 750); 
             msleep(1000);
+            ao();
+            msleep(100);
+            
+			//turn around
             cmpc(3);
-            while(gmpc(3) > -1000)
-            {
-                motor(0, 100);
-                motor(3, -100);
-            }//make a turn
-            ao(); //stop after the turn
-            
-            set_servo_position(2, 1000);//drop the noodle
-            msleep(500);
-            
-            while(gmpc(3) < -310)
+            while(gmpc(3) < 2250)
             {
                 motor(0, -100);
                 motor(3, 100);
             }
+            ao(); 
+            
+            motor(0, 100); //go forward towards data center
+            motor(3, 100);
+            msleep(1500);
             ao();
+
+                
+            msleep(1000);
+
+            set_servo_position(1, 750);
+            msleep(200);
+            set_servo_position(2, 1000);//drop the noodle
+            msleep(500);
             
-            //get the poms GET THE POMS POOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOMS
-		
-	    ////VERY VERY VERY VERY UNFISHED AND NEEDS FURTHER TESTING AND PROGRAMMING
-            if(analog(1) > 3600)
+            cmpc(3); //turn around (again)
+            while(gmpc(3) < 2000)
             {
-                msleep(500); //move for a little bit to get the sensor centered on the line
-                ao(); //stop moving
+                motor(0, -100);
+                motor(3, 100);
             }
-            else
-            {
-                motor(0, 70); //move when not seeing black line
-                motor(3, 70);
-            }
-            //after hitting line
-            cmpc(3);
-            while(gmpc(3) < 850)
-            {
-                motor(0, 100);
-                motor(3, -100);
-            }
-            ao();//after the turn
+            ao(); 
             
-            msleep(100);
-            motor(0, 50);
-            motor(3, 50); // placeholder code for a line follower
-            
-          }
+            //get the poms GET THE POMS POOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOMS or noodles again
+        }
         else
         {
-            motor(0, 70);
-            motor(3, 70);
+            motor(0, 100);
+            motor(3, 100);
         }
+		
 	}
     return 0;
 }
