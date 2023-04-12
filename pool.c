1c992fd3d5dc77a35dc6a247830254a918a30dff
AHSBotball <129643474+AHSBotball@users.noreply.github.com>
2023-04-12 14:50:43 -0400

    Update some code and add more code
    The code past 77 doesn't really work and I don't know why but when it does it should be good. I added some more delays. I also changed the beginning turn to save more time and to be more accurate.

--- pool.c
+++ pool.c
@@ -3,7 +3,7 @@
 int main()
 {
     wait_for_light(3);
-    shut_down_in(115);
+    shut_down_in(100);
 	
     msleep(5000); //delay for icreate to start
 	
@@ -12,30 +12,33 @@ int main()
     enable_servo(2);
     set_servo_position(1,600);
     set_servo_position(2, 1600);
-   
-        while (gmpc(3)<1720) // goes forward a bit 
-        {
-            motor(0,100);
-            motor(3,100);
-        }
+    
+    //START of movement
+    cmpc(3);
+    while (gmpc(3)<1500)
+    {
+        motor(3,100); //go forward a lil bit
+        motor(0,100);
+    } 
     ao();
-    msleep(1000); // pause for a bit
+    msleep(100);
     
     cmpc(3);
-    while (gmpc(3)<4200)
+    while (gmpc(3)<2080)
     {
-        motor(3,100); // turn 270 deg
-    }
+        motor(3,80); //turn around
+        motor(0,-80);
+    } 
     ao();
-    msleep(1000);
+    msleep(100);
     
     while(digital(0) == 0) // will always equal 0
     { 
         if(analog(0) > 2900) //if proximity sensor close 
         {
-            msleep(400);// pause 
+            msleep(350);// pause 
             ao();
-            msleep(2000);
+            msleep(500);
             set_servo_position(2, 2000); // close the claw
             msleep(500);
             set_servo_position(1, 0); //lift the noodle
@@ -49,35 +52,78 @@ int main()
             
 			//turn around
             cmpc(3);
-            while(gmpc(3) < 2250)
+            while(gmpc(3) < 2150)
             {
                 motor(0, -100);
                 motor(3, 100);
             }
             ao(); 
-            
+            msleep(100);
             motor(0, 100); //go forward towards data center
             motor(3, 100);
             msleep(1500);
             ao();
 
                 
-            msleep(1000);
+            msleep(500); //wait a bit
 
             set_servo_position(1, 750);
-            msleep(200);
+            msleep(500); //delay as to not have to much momentum when dropping the noodle
             set_servo_position(2, 1000);//drop the noodle
             msleep(500);
+            set_servo_position(1, 600); //lift the claw back up to prepare to get another noodle
+            msleep(500);
             
-            cmpc(3); //turn around (again)
-            while(gmpc(3) < 2000)
+            cmpc(0); //NOTHING WORKS PAST HERE
+            //NOTHING WORKS PAST HERE
+            //NOTHING WORKS PAST HERE
+            
+            while(gmpc(0) < 1000)
             {
-                motor(0, -100);
+                motor(3, -100); //turn left
+                motor(0, 100);
+            }
+            ao();
+            msleep(100); //pause for a bit
+            
+            motor(0, 100);
+            motor(3, 50);
+            msleep(500);
+            
+            //go back to get the noodles NEW CODE AS OF 4/12/23
+            
+            while (analog(2) < 3700)
+            {
+                motor(0, 100); //move forwards until line is seen
                 motor(3, 100);
             }
-            ao(); 
+			ao();
+            msleep(100);
             
-            //get the poms GET THE POMS POOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOMS or noodles again
+            cmpc(0); //turn left towards line
+            while(gmpc(0) < 1500)
+            {
+                motor(3, -100);
+                motor(0, 100);
+            }
+            ao();
+            
+            //enter robot into loop where it does a line follwing until it is close to noodle
+            
+            if(analog(0) < 2900) //if proximity sensor far
+            {
+                if(analog(2) < 3700) //if robot not on line 
+                {
+                    motor(3, 70); //turn right
+                    motor(1, 20);
+                }
+                else //on line
+                {
+                    motor(0, 100); // go forwards
+                    motor(3, 100);
+                }
+            }
+                  
         }
         else
         {
