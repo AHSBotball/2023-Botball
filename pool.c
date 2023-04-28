c63be9e745b56f20ada0089e7a96c1d7e2103cf2
AHSBotball <129643474+AHSBotball@users.noreply.github.com>
2023-04-28 14:22:23 -0400

    Changed some functions and fixed stuff
    Added more to the dropNoodle() function so I didn't have too much repeating code. Altered some values to make it more consistent.

--- pool.c
+++ pool.c
@@ -11,7 +11,7 @@ void turn(int side, int value, int lSpeed, int rSpeed) //create a function to ma
     ao();
 }
 
-void grabNoodle()
+void grabNoodle() //function to grab noodle
 {
     set_servo_position(2, 2000); // close the claw
     msleep(500);
@@ -19,8 +19,14 @@ void grabNoodle()
     msleep(1500);
 }
 
-void dropNoodle()
+void dropNoodle() //function to drop noodle
 {
+    motor(0, 100); //go forwards towards data center
+    motor(3, 100);
+    msleep(2000);
+    ao();        
+    msleep(500); //wait a bit
+    
 	set_servo_position(1, 1200);
     msleep(500); //delay as to not have to much momentum when dropping the noodle
     set_servo_position(2, 1200);//drop the noodle
@@ -33,7 +39,7 @@ void dropNoodle()
 
 int main()
 {
-    //wait_for_light(3);
+    wait_for_light(3);
     shut_down_in(100);
 	
     cmpc(3); 
@@ -55,7 +61,7 @@ int main()
     
     msleep(100);
     
-    turn(3, 2000, 80, -80); //turn around to get the first noodle
+    turn(3, 2050, 80, -80); //turn around to get the first noodle
 
     ao();
     msleep(100);
@@ -77,30 +83,22 @@ int main()
             
 			//turn around
             
-            turn(3, 2300, 100, -100);
+            turn(3, 2150, 100, -100);
             
             msleep(100);
-			//go forwards towards data center
-            motor(0, 100);
-            motor(3, 100);
-            msleep(2300);
-            ao();
-                
-            msleep(500); //wait a bit
             
             //DROP THE NOODLE
-            
             dropNoodle();
             
-            turn(0, 740, -100, 100); //turn to face the line
+            turn(0, 800, -100, 100); //turn to face the line
 
             msleep(100); //pause for a bit
             
             
             while (analog(1) < 3700)
             {
-                motor(0, 100); //move forwards until line is seen
-                motor(3, 100);
+                motor(0, 80); //move forwards until line is seen
+                motor(3, 80);
             }
 			ao();
             msleep(300);
@@ -125,12 +123,12 @@ int main()
             }
             //grab the noodle again
             printf("0");
-            msleep(100);// pause to get closer to noodle
+            msleep(100);
             motor(0, 50);
-            motor(3, 50);
-            msleep(1000);
+            motor(3, 50);//move forward slowly when in front of noodle
+            msleep(1000); //delay to get closer to noodle
             ao();
-            msleep(400);
+            msleep(500);
 			grabNoodle();
             
             //DROP IT OFF (AGAIN)
@@ -141,22 +139,14 @@ int main()
             ao();
             msleep(100);
             
-			//turn around
+	    //turn around
             
-            turn(3, 2450, 100, -100);
+            turn(3, 2500, 100, -100);
             msleep(100);
             
-			//go forwards towards data center
-            motor(0, 100);
-            motor(3, 100);
-            msleep(2000);
-            ao();
-                
-            msleep(500); //wait a bit
-
 			dropNoodle();
             
-            //GET THRID ONE AFTER THIS
+            //GET THIRD ONE
             //return 0; //uncomment this line to stop code after second noodle is gotten
                      
             turn(0, 2600, -20, 100); //turn left towards line
@@ -165,14 +155,14 @@ int main()
             
             while (analog(1) < 3700)
             {
-                motor(0, 100); //move forwards until line is seen
-                motor(3, 100);
+                motor(0, 80); //move forwards until line is seen
+                motor(3, 80);
             }
-            msleep(400);
+            msleep(50); //slight delay to be over the line slightly
             ao();
             msleep(100);
 			//turn left towards final noodle
-			turn(0, 2100, 0, 100);
+			turn(0, 2000, 0, 100);
             
             while(analog(0) < 2800) //if proximity sensor far
             {
@@ -189,8 +179,9 @@ int main()
                 }
             }
             ao();
-            motor(3, 50);
-            msleep(1200);// pause to get closer to noodle
+            motor(0, 20);
+            motor(3, 50); //drive to the right to grab the final noodle after the empty rod is detected
+            msleep(1400);// delay to get closer to noodle
             ao();
             msleep(400);
 			grabNoodle();
@@ -203,17 +194,9 @@ int main()
             
 			//turn around
             
-            turn(3, 2400, 100, -100);
+            turn(3, 2000, 100, -100);
             msleep(100);
             
-			//go forwards towards data center
-            motor(0, 100);
-            motor(3, 100);
-            msleep(2000);
-            ao();
-                
-            msleep(500); //wait a bit
-
 			dropNoodle();
             
             return 0;
